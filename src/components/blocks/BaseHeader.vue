ce885d2b1ace94f41f249e5b2c9e6e60cf3b1952
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-09 12:47:56 +0200

    add movies

--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -1,12 +1,47 @@
 <template>
     <header>
-        <h1>ciao</h1>
+        <h1>Hello</h1>
+        <div class="research">
+            <form action="">
+                <input type="text" placeholder="Search">
+                <button>Search</button>
+            </form>
+        </div>
+        <div class="movies__results">
+            <ul>
+                <li v-for="(movie, index) in movies" :key="index">{{movie.title}}</li>
+            </ul>
+        </div>
     </header>
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
-        
+        name: 'BaseHeader',
+        data () {
+            return {
+                movies: [],
+            }
+        },
+        created() {
+            axios.get("https://api.themoviedb.org/3/search/movie",
+                {
+                    params: {
+                        api_key: '4adbf89faf8e1206e88d749b8cee0388',
+                        query: 'godzilla',
+                        language: 'it-IT',
+                    }
+                }
+            )
+            .then((response) => {
+                this.movies = response.data.results;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
     }
 </script>
 
