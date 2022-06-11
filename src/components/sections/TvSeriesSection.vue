40dd1493b1aac9b3a29ef343a87b46b9a7a73cd7
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-11 18:58:56 +0200

    add tv series flag

--- src/components/sections/TvSeriesSection.vue
+++ src/components/sections/TvSeriesSection.vue
@@ -7,7 +7,7 @@
                 <h3>{{tvSerie.name}}</h3>
                 <h4>{{tvSerie.original_name}}</h4>
                 <h5>{{tvSerie.vote_average}}</h5>
-                <h6>{{tvSerie.original_language}}</h6>
+                <img class="flag" :src="seriesFlag(tvSerie.original_language)" alt="language-flag">
             </li>
         </ul>
     </div>
@@ -21,8 +21,42 @@
         data () {
             return {
                 PageArray,
+                DataFlags: {
+                    it: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/280px-Flag_of_Italy.svg.png",
+                    en: "https://www.mlaworld.com/wp-content/uploads/2021/07/metà-bandiera-americana-e-inglese.png",
+                    es: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/280px-Flag_of_Spain.svg.png",
+                    fr: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_France_%281794–1815%2C_1830–1974%2C_2020–present%29.svg/280px-Flag_of_France_%281794–1815%2C_1830–1974%2C_2020–present%29.svg.png",
+                    de: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/280px-Flag_of_Germany.svg.png",
+                    ja: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/280px-Flag_of_Japan.svg.png",
+                    ko: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/280px-Flag_of_South_Korea.svg.png",
+                    tr: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/280px-Flag_of_Turkey.svg.png",
+                    world: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Flag_of_the_United_Nations.svg/220px-Flag_of_the_United_Nations.svg.png",
+                },
             }
-        }
+        },
+        methods: {
+            seriesFlag(language) {
+                if (language === "it") {
+                    return this.DataFlags.it
+                } else if (language === "en") {
+                    return this.DataFlags.en
+                } else if (language === "es") {
+                    return this.DataFlags.es
+                } else if (language === "fr") {
+                    return this.DataFlags.fr
+                } else if (language === "de") {
+                    return this.DataFlags.de
+                } else if (language === "ja") {
+                    return this.DataFlags.ja
+                } else if (language === "ko") {
+                    return this.DataFlags.ko
+                } else if (language === "tr") {
+                    return this.DataFlags.tr
+                } else {
+                    return this.DataFlags.world
+                }
+            },
+        },
     }
 </script>
 
@@ -30,5 +64,8 @@
     .series {
         background-color: #0339fc;
         color: #fff;
+        .flag {
+            max-width: 100px;
+        }
     }    
 </style>
